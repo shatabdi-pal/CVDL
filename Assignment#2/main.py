012e62bf994179ac63599412ee88ff9426339c4f
shatabdi-pal <shatabdi@pdx.edu>
2023-07-16 17:06:39 -0700

    Lucas-Kanade method updated

--- Assignment#2/main.py
+++ Assignment#2/main.py
@@ -12,6 +12,7 @@ def load_image(image_path):
     return image_array
 
 
+
 def spatial_gradient(frame1):
     gx_filter = np.array([[1, 0, -1],
                           [2, 0, -2],
@@ -22,17 +23,18 @@ def spatial_gradient(frame1):
     padding = 1
     padded_image = np.pad(frame1, padding, mode="constant")
 
-    Ix = np.zeros_like(frame1, dtype=np.float32)
-    Iy = np.zeros_like(frame1, dtype=np.float32)
+    Ix = np.zeros_like(frame1)
+    Iy = np.zeros_like(frame1)
+
     frame1_height, frame1_width = frame1.shape
     for i in range(1, frame1_height + 1):
         for j in range(1, frame1_width + 1):
             Ix[i -1, j - 1] = np.sum(padded_image[i - 1: i + 2, j - 1 : j + 2]* gx_filter)
             Iy[i - 1, j - 1] = np.sum(padded_image[i - 1: i + 2, j - 1: j + 2] * gy_filter)
-            
+
     # #Normalizing output
-    Ix = (Ix- Ix.min()) / (Ix.max() - Ix.min())
-    Iy = (Iy -Iy.min()) / (Iy.max() -Iy.min())
+    # Ix = (Ix- Ix.min()) / (Ix.max() - Ix.min())
+    # Iy = (Iy -Iy.min()) / (Iy.max() -Iy.min())
     return Ix,Iy
 
 def lucas_kanade_method(frame1, frame2):
@@ -41,25 +43,29 @@ def lucas_kanade_method(frame1, frame2):
 
     # Compute temporal derivative
     It = frame2 - frame1
-    print(It)
+
     # Compute optical flow vectors
     Vx = np.zeros_like(frame1)
     Vy = np.zeros_like(frame1)
 
-    kernel = np.ones((3, 3))
-
-    for y in range(1, frame1.shape[0] - 1):
-        for x in range(1, frame1.shape[1] - 1):
-            Ix_patch = Ix[y - 1:y + 2, x - 1:x + 2].flatten()
-            Iy_patch = Iy[y - 1:y + 2, x - 1:x + 2].flatten()
-            It_patch = It[y - 1:y + 2, x - 1:x + 2].flatten()
+    padded_frame1 = np.pad(frame1, pad_width=1, mode="constant")
+    padded_Ix = np.pad(Ix, pad_width=1, mode="constant")
+    padded_Iy = np.pad(Iy, pad_width=1, mode="constant")
+    padded_It = np.pad(It, pad_width=1, mode="constant")
 
 
-    A = np.vstack((Ix_patch, Iy_patch)).T
-    b = -It_patch
-    optical_flow = np.linalg.lstsq(A, b, rcond=None)[0]
-    Vx[y, x] = optical_flow[0]
-    Vy[y, x] = optical_flow[1]
+    for y in range(frame1.shape[0]):
+        for x in range(frame1.shape[1]):
+            # Extract patch around the pixel
+            Ix_patch = padded_Ix[y : y + 3, x : x + 3].flatten()
+            Iy_patch = padded_Iy[y : y + 3, x : x + 3].flatten()
+            It_patch = padded_It[y : y + 3, x : x + 3].flatten()
+
+            A = np.vstack((Ix_patch, Iy_patch)).T
+            b = -It_patch
+            optical_flow = np.linalg.lstsq(A, b, rcond=None)[0]
+            Vx[y, x] = optical_flow[0]
+            Vy[y, x] = optical_flow[1]
 
     return Vx, Vy
 
@@ -73,18 +79,43 @@ if __name__ == "__main__":
     frame_1_b = load_image(path_1_a)
     frame_2_a = load_image(path_2_a)
     frame_2_b = load_image(path_2_b)
-    Vx, Vy = lucas_kanade_method(frame_2_a, frame_2_b)
-
+    # Calculate optical flow
+    Vx_1, Vy_1 = lucas_kanade_method(frame_1_a, frame_1_b)
+    Vx_2, Vy_2 = lucas_kanade_method(frame_2_a, frame_2_b)
 
     # Visualize optical flow vectors
-    plt.figure(figsize=(10, 5))
-    plt.quiver(Vx, Vy, color='r')
-    plt.title('Optical Flow Vectors')
+    plt.figure(figsize=(12, 6))
+
+    plt.subplot(121)
+    plt.quiver(Vx_1, Vy_1, color='k')
+    plt.title('Optical Flow Vectors (Frame 1)')
+    plt.axis('off')
+
+    plt.subplot(122)
+    plt.quiver(Vx_2, Vy_2, color='k')
+    plt.title('Optical Flow Vectors (Frame 2)')
+    plt.axis('off')
+
+    plt.tight_layout()
     plt.show()
 
     # Visualize magnitude of optical flow
-    magnitude = np.sqrt(Vx ** 2 + Vy ** 2)
-    plt.imshow(magnitude, cmap='hot')
-    plt.title('Magnitude of Optical Flow')
+    magnitude_1 = np.sqrt(Vx_1 ** 2 + Vy_1 ** 2)
+    magnitude_2 = np.sqrt(Vx_2 ** 2 + Vy_2 ** 2)
+
+    plt.figure(figsize=(12, 6))
+
+    plt.subplot(121)
+    plt.imshow(magnitude_1, cmap='viridis')
+    plt.title('Magnitude of Optical Flow (Frame 1)')
     plt.colorbar()
+    plt.axis('off')
+
+    plt.subplot(122)
+    plt.imshow(magnitude_2, cmap='viridis')
+    plt.title('Magnitude of Optical Flow (Frame 2)')
+    plt.colorbar()
+    plt.axis('off')
+
+    plt.tight_layout()
     plt.show()
