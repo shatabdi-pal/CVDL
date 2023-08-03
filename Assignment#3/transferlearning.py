2cd2510b38e16fd0bf1ec37315bccc14eee32892
shatabdi-pal <shatabdi@pdx.edu>
2023-08-03 16:19:00 -0700

    Implemention for programming#3 updated

--- Assignment#3/transferlearning.py
+++ Assignment#3/transferlearning.py
@@ -1,4 +1,3 @@
-
 import os
 import cv2
 import numpy as np
@@ -7,12 +6,13 @@ from keras.models import Sequential
 from keras.utils import to_categorical
 from sklearn.preprocessing import LabelEncoder
 from keras.applications import InceptionResNetV2
-from keras.layers import Dense, GlobalAveragePooling2D
+from keras.layers import Dense
 from keras.optimizers import Adam
 from sklearn.metrics import accuracy_score, confusion_matrix
+from keras import layers
+from keras import models
+from keras.layers import Flatten
 from keras.models import Model
-from keras.layers import Input
-
 
 
 #Load the InceptionResNetv2 model
@@ -24,17 +24,20 @@ pre_model.summary()
 # Access the first convolutional layer (index 1)
 first_conv_layer = pre_model.layers[1]
 first_layer_weights = first_conv_layer.get_weights()[0]
+print(first_layer_weights.shape)
 
 # Reshape and plot the filters
 n_filters = first_layer_weights.shape[3]
 n_channels = first_layer_weights.shape[2]
 filter_size = first_layer_weights.shape[0]
 
+print()
+
 plt.figure(figsize=(8, 8))
 for i in range(n_filters):
     ax = plt.subplot(8, 8, i + 1)
-    filter_img = first_layer_weights[:, :, :, i]
-    filter_img = filter_img.reshape(filter_size, filter_size, n_channels)
+    filter_img = first_layer_weights[:, :, :, i] #Acess the weigth of the ith filter
+    filter_img = filter_img.reshape(filter_size, filter_size, n_channels) #each filter convert in 3d array
     # Normalize the filter values
     filter_img = (filter_img - np.min(filter_img)) / (np.max(filter_img) - np.min(filter_img))
     plt.imshow(filter_img[:, :, 0], cmap='gray')
@@ -75,6 +78,13 @@ train_labels = np.array(train_cat_labels + train_dog_labels)
 test_data = np.array(test_cat_data + test_dog_data)
 test_labels = np.array(test_cat_labels + test_dog_labels)
 
+# Initialize the LabelEncoder
+label_encoder = LabelEncoder()
+
+# Fit and transform the string labels to integer labels
+train_labels = label_encoder.fit_transform(train_labels)
+test_labels = label_encoder.fit_transform(test_labels)
+
 # Shuffle the training data and labels
 indices = np.arange(train_data.shape[0])
 np.random.shuffle(indices)
@@ -85,18 +95,6 @@ train_labels = train_labels[indices]
 train_data = train_data.astype('float32') / 255.0
 test_data = test_data.astype('float32') / 255.0
 
-# Initialize the LabelEncoder
-label_encoder = LabelEncoder()
-
-# Fit and transform the string labels to integer labels
-train_labels = label_encoder.fit_transform(train_labels)
-test_labels = label_encoder.fit_transform(test_labels)
-
-#Convert labels to categorical format (one-hot encoding)
-num_classes = 2  # cat and dog
-train_labels = to_categorical(train_labels, num_classes)
-test_labels = to_categorical(test_labels, num_classes)
-
 
 # Load the pre-trained InceptionResNetV2 model with weights trained on the ImageNet dataset
 pre_model = InceptionResNetV2(weights='imagenet', include_top=False, input_shape=(150, 150, 3))
@@ -105,23 +103,21 @@ pre_model = InceptionResNetV2(weights='imagenet', include_top=False, input_shape
 pre_model.trainable = False
 
 # Create the new model (transfer head)
-model = Sequential()
+model = models.Sequential()
 
 # Add the pre-trained InceptionResNetV2 as the base of the model
 model.add(pre_model)
 
-# Add a Global Average Pooling layer to reduce spatial dimensions
-model.add(GlobalAveragePooling2D())
+# Add a flatten layer
+model.add(layers.Flatten())
 
 # Add a new classification head (dense layer) for your specific task
-# For example, let's add a dense layer with 256 units and a softmax layer for binary classification (cats vs. dogs)
-model.add(Dense(256, activation='relu'))
-model.add(Dense(2, activation='softmax'))
+# add a dense layer with 256 units and a sigmoid for binary classification (cats vs. dogs)
+model.add(layers.Dense(256, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
 
 # Print the summary of the new model
 model.summary()
-
-
 
 
 # Step 1: Evaluate the transfer model (without training the unfrozen weights) on the test dataset
@@ -138,10 +134,10 @@ print(f'Overall Test Accuracy: {accuracy}')
 # Create predictions for the test data
 predictions = model.predict(test_data)
 predicted_labels = np.argmax(predictions, axis=1)
-true_labels = np.argmax(test_labels, axis=1)
+
 
 # Create and print the confusion matrix
-confusion_mat = confusion_matrix(true_labels, predicted_labels)
+confusion_mat = confusion_matrix(test_labels, predicted_labels)
 print('Confusion Matrix:')
 print(confusion_mat)
 
@@ -167,7 +163,7 @@ predictions = model.predict(test_data)
 predicted_labels = np.argmax(predictions, axis=1)
 
 # Create and plot the confusion matrix for the final model
-confusion_mat = confusion_matrix(true_labels, predicted_labels)
+confusion_mat = confusion_matrix(test_labels, predicted_labels)
 print('Confusion Matrix (Final Model):')
 print(confusion_mat)
 
@@ -180,9 +176,8 @@ plt.legend()
 plt.show()
 
 
-# Assuming you have the pre-trained InceptionResNetV2 model "pre_model" from Step 1
-
 # Choose the number of layers to retain in the sub-network (k layers)
+
 k = 100
 
 # Create a new input layer for the sub-network with the same input shape as the pre-trained model
@@ -194,10 +189,10 @@ sub_model = Model(inputs=pre_model.input, outputs=pre_model.layers[k].output)
 # Connect the new input layer to the output of the kth layer of the pre-trained model
 sub_output = sub_model(sub_input)
 
-# Add a Global Average Pooling layer and a new classification head to the sub-network
-x = GlobalAveragePooling2D()(sub_output)
+# Add a Flatten layer and a new classification head to the sub-network
+x = Flatten()(sub_output)
 x = Dense(256, activation='relu')(x)
-sub_model_output = Dense(2, activation='softmax')(x)
+sub_model_output = Dense(1, activation='sigmoid')(x)
 
 # Create the final sub-network model with the input and output layers
 sub_model = Model(inputs=sub_input, outputs=sub_model_output)
@@ -221,14 +216,21 @@ loss_sub, accuracy_sub = sub_model.evaluate(test_data, test_labels)
 # Print the per-epoch test loss for the sub-network model
 print('Per-epoch Test Loss for Sub-Network:')
 print(loss_sub)
-
 # Create predictions for the test data using the sub-network model
 predictions_sub = sub_model.predict(test_data)
 predicted_labels_sub = np.argmax(predictions_sub, axis=1)
 
+
 # Create and print the confusion matrix for the sub-network model
-confusion_mat_sub = confusion_matrix(true_labels, predicted_labels_sub)
+confusion_mat_sub = confusion_matrix(test_labels, predicted_labels_sub)
 print('Confusion Matrix for Sub-Network Model:')
 print(confusion_mat_sub)
 
+# Plot the training and validation loss for each epoch
+plt.plot(history.history['loss'], label='Training Loss')
+plt.plot(history.history['val_loss'], label='Validation Loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
 
